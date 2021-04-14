adb6b998d2bb1c1bbfddebb7e4ddce5f275b07c9
Augusto Nunes <nunes.augusto19@gamil.com>
2021-04-14 00:23:55 -0300

    adds semestre and fwd; update aulas

--- src/main.py
+++ src/main.py
@@ -3,14 +3,34 @@ from pendulum import today, datetime
 import random
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Bot do BECD")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Já em funcionamento.")
 
-"""
+
 def fwd(update, context):
-
-"""
+    is_reply = update.message.reply_to_message
+    fwd_name = '@becdbotfwd'
+    
+    if is_reply: # encaminha para o fwd
+        context.bot.forwardMessage(fwd_name, update.effective_chat.id, is_reply.message_id)
+    else: # encaminha para o grupo
+        max_id = 10
+        max_tries = max_id*5
+        tries = 0
+        while True:
+            message_id = random.randint(0, max_id)
+            try:
+                if tries <= max_tries: 
+                    context.bot.forwardMessage(update.effective_chat.id, fwd_name, message_id )
+                    break
+                    tries+=1
+                else:
+                    context.bot.send_message(chat_id=update.effective_chat.id, text="Ai cansei aqui, manda de novo...")
+            except:
+                pass
+    
 
 def aulas(update, context):
+    """
     hoje = today('America/Sao_Paulo')
     aulas = datetime(2021, 4, 12)
     diff = aulas.diff(hoje).in_days()
@@ -22,20 +42,32 @@ def aulas(update, context):
     ]
     frase = random.choice(frases)
     msg = f'{frase} {diff} dias'
+    """
+    msg = "As aulas já começaram, manda /semestre pra saber quando o semestre vai dar a folguinha."
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+
+def semestre(update, context):
+    hoje = today('America/Sao_Paulo')
+    aulas = datetime(2021, 7, 4)
+    diff = aulas.diff(hoje).in_days()
+    frases = [
+        'Por favor, eu não aguento mais {} dias'
+    ]
+    frase = random.choice(frases)
+    msg = frase.format(diff)
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def maquera(update, context):
     context.bot.forward_message(chat_id=update.effective_chat.id, from_chat_id='@becdbotfwd', message_id=2)
 
+
+
 def send_welcome(update, context, new_member):
-    welcome_message = """Bem-vindeee, @[{}](tg://user?id={})!
-Se apresenta pra gente, é bixo ou veterano? 
-Arroz em baixo ou em cima do feijão?
+    welcome_message = """Bem-vinde, [@{}](tg://user?id={})!
     """.format(new_member.first_name, new_member.id)
 
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=welcome_message, parse_mode='Markdown')
-
 
 def empty_message(update, context):
     """
@@ -63,11 +95,7 @@ if __name__ == '__main__':
                         default=False,
                         help='Local run')
     args = parser.parse_args()
-
-    if not args.is_local:
-        BOT_API_TOKEN = os.getenv('BOT_API_TOKEN')
-    else:
-        pass
+    BOT_API_TOKEN = os.getenv('BOT_API_TOKEN')
     
     updater = Updater(token=BOT_API_TOKEN, use_context=True)
 
@@ -76,9 +104,12 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('aulas', aulas))
     dp.add_handler(CommandHandler('maquera', maquera))
+    dp.add_handler(CommandHandler('fwd', fwd))
+    dp.add_handler(CommandHandler('semestre', semestre))
     dp.add_handler(MessageHandler(Filters.status_update, empty_message))
     
-    if not args.is_local:
+    
+    if not True:#args.is_local:
         PORT = os.getenv('PORT')
 
         updater.start_webhook(listen="0.0.0.0",
---
+++ venv.sh
@@ -0,0 +1,3 @@
+python -m venv ./bot_venv
+source ./bot_venv/bin/activate
+pip install -r ./requirements.txt
